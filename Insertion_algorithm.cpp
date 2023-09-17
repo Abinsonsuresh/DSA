efab7da5ade3774398aecd97b305581bbcd023f1
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2023-09-17 21:02:28 +0530

    array insertion completed

--- Insertion_algorithm.cpp
+++ Insertion_algorithm.cpp
@@ -1,8 +1,54 @@
-#include<iostream>
 // #include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-int main(){
-    cout<<"Hello world";
+void display(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<"\n";
+
+}
+
+void insertion(int arr[], int size, int element, int position)
+{
+    for(int i = size - 1; i>=position ; i--)
+    {
+        arr[i+1] = arr[i];
+    }
+    arr[position] = element;
+    cout<<"\n inserted array is : ";
+    for (int i = 0; i < size; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
+
+int main()
+{
+    int i, arr[100], n, element, position;
+    cout << "\nEnter the no. of elements in the array ";
+    cin >> n;
+    cout << "\nEnter the elements: ";
+    for (i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    display(arr, n);
+
+    cout << "\nEnter the element to insert into the array ";
+    cin >> element;
+
+    cout << "\nEnter the position of the element to insert into the array ";
+    cin >> position;
+
+    n = n + 1;
+    insertion(arr, n, element, position);
+    // display(arr, n);
+
+
     return 0;
 }
